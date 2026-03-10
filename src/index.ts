3ef80c9d81e6ae54dc000daf06922ef61c6b9510
Binod Chaudhary <thebinod7@gmail.com>
2026-03-10 11:53:17 +0545

    added config env

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
 import express from "express";
+import dotenv from "dotenv";
+import { env } from "./config/env";
+
+dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
-    message: "Hello from Express + TypeScript 🚀",
+    message: "Hello from Express + TypeScript. This is " + env.APP_NAME,
   });
 });
 
-app.listen(PORT, () => {
+app.listen(env.PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
