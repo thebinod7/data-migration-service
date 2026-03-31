03759a19880d9b25957dffd02bf00a753f851a0a
Binod Chaudhary <thebinod7@gmail.com>
2026-03-31 18:02:29 +0545

    added personal impact read query

--- src/db-migration.ts
+++ src/db-migration.ts
@@ -2,7 +2,10 @@ import "dotenv/config";
 
 import { ConvexHttpClient } from "convex/browser";
 import { api } from "../convex/_generated/api";
-import { closeCertificateMysqlPool } from "./extractors/certificate_app";
+import {
+  closeCertificateMysqlPool,
+  listPersonalImpactPages,
+} from "./extractors/certificate_app";
 import { closeWordpressMysqlPool, listWpUsers } from "./extractors/wp_app";
 import { splitFullName } from "./utils/utils";
 
@@ -11,54 +14,19 @@ const LAST_SEEN_ID = 0;
 
 const convex = new ConvexHttpClient(process.env.CONVEX_URL!);
 
-async function runMigration(): Promise<void> {
-  const ctx = {
-    emailToUserId: new Map<string, string>(),
-    wpIdToUserId: new Map<number, string>(),
-    userToAccounts: new Map<string, string[]>(),
-  };
+const ctx = {
+  emailToUserId: new Map<string, string>(),
+  wpIdToUserId: new Map<number, string>(),
+  userToAccounts: new Map<string, string[]>(),
+};
 
+async function runMigration(): Promise<void> {
   try {
     // ---------------- USERS ----------------
-    for await (const batch of listWpUsers(LAST_SEEN_ID, BATCH_SIZE)) {
-      const users = batch.map((wp: any) => {
-        const { firstName, lastName } = splitFullName(
-          wp.display_name || wp.user_login,
-        );
-        return {
-          ssoUserId: `wp-${wp.ID}`, // TODO: get ssoId from Auth Server
-          email: wp.user_email,
-          firstName: firstName,
-          lastName: lastName,
-          wordpressUserId: wp.ID,
-          role: "user" as const,
-          onboardingCompleted: true,
-          signupSideEffectsCompleted: true,
-          createdAt: new Date(wp.user_registered).getTime(),
-          updatedAt: Date.now(),
-        };
-      });
-
-      console.log(`Migrating batch of ${users.length} users...`);
-      const result = await convex.mutation(api.migrations.bulkInsertUsers, {
-        records: users,
-      });
-
-      if (!result || !Array.isArray(result)) {
-        console.error("Failed to insert users batch", { result });
-        continue;
-      }
-
-      result.forEach((r: any, i: number) => {
-        const u = users[i];
-        ctx.emailToUserId.set(u.email, r.id);
-        ctx.wpIdToUserId.set(u.wordpressUserId, r.id);
-      });
-
-      console.log(`✅ Inserted ${users.length} users`);
-    }
+    // await migrateUsersFromWordpress();
 
     // ---------------- ACCOUNTS ----------------
+    await migrateImpactAccounts();
 
     console.log("✅ Migration completed!");
   } catch (err: any) {
@@ -74,3 +42,53 @@ async function runMigration(): Promise<void> {
 }
 
 runMigration();
+
+async function migrateImpactAccounts() {
+  const personalImpacts = listPersonalImpactPages(LAST_SEEN_ID, BATCH_SIZE);
+  const businessImpacts = listPersonalImpactPages(LAST_SEEN_ID, BATCH_SIZE);
+
+  // Prepare data for peronal impacts
+  for await (const batch of personalImpacts) {
+    console.log("Batch of personal impacts", batch);
+  }
+}
+
+async function migrateUsersFromWordpress() {
+  for await (const batch of listWpUsers(LAST_SEEN_ID, BATCH_SIZE)) {
+    const users = batch.map((wp: any) => {
+      const { firstName, lastName } = splitFullName(
+        wp.display_name || wp.user_login,
+      );
+      return {
+        ssoUserId: `wp-${wp.ID}`, // TODO: get ssoId from Auth Server
+        email: wp.user_email,
+        firstName: firstName,
+        lastName: lastName,
+        wordpressUserId: wp.ID,
+        role: "user" as const,
+        onboardingCompleted: true,
+        signupSideEffectsCompleted: true,
+        createdAt: new Date(wp.user_registered).getTime(),
+        updatedAt: Date.now(),
+      };
+    });
+
+    console.log(`Migrating batch of ${users.length} users...`);
+    const result = await convex.mutation(api.migrations.bulkInsertUsers, {
+      records: users,
+    });
+
+    if (!result || !Array.isArray(result)) {
+      console.error("Failed to insert users batch", { result });
+      continue;
+    }
+
+    result.forEach((r: any, i: number) => {
+      const u = users[i];
+      ctx.emailToUserId.set(u.email, r.id);
+      ctx.wpIdToUserId.set(u.wordpressUserId, r.id);
+    });
+
+    console.log(`✅ Inserted ${users.length} users`);
+  }
+}
--- src/extractors/certificate_app.ts
+++ src/extractors/certificate_app.ts
@@ -19,6 +19,67 @@ export function getMysqlPool(): mysql.Pool {
   return pool;
 }
 
+export async function* listImpactPages(
+  lastSeenId: number = 0,
+  batchSize: number = 5,
+): AsyncGenerator<Record<string, unknown>[]> {
+  const pool = getMysqlPool();
+
+  let currentId = Math.max(0, Number(lastSeenId) || 0);
+  const limit = Math.max(1, Number(batchSize) || 5);
+
+  while (true) {
+    const [rows] = await pool.execute<mysql.RowDataPacket[]>(
+      `
+      SELECT *
+      FROM impact_pages
+      WHERE id > ?
+      ORDER BY id ASC
+      LIMIT ${limit}
+      `,
+      [currentId],
+    );
+
+    const batch = rows as Record<string, unknown>[];
+
+    if (batch.length === 0) break;
+
+    yield batch;
+
+    currentId = batch[batch.length - 1].id as number;
+  }
+}
+export async function* listPersonalImpactPages(
+  lastSeenId: number = 0,
+  batchSize: number = 5,
+): AsyncGenerator<Record<string, unknown>[]> {
+  const pool = getMysqlPool();
+
+  let currentId = Math.max(0, Number(lastSeenId) || 0);
+  const limit = Math.max(1, Number(batchSize) || 5);
+
+  while (true) {
+    const [rows] = await pool.execute<mysql.RowDataPacket[]>(
+      `
+      SELECT *
+      FROM personal_impact_pages
+      WHERE id > ?
+      ORDER BY id ASC
+      LIMIT ${limit}
+      `,
+      [currentId],
+    );
+
+    const batch = rows as Record<string, unknown>[];
+
+    if (batch.length === 0) break;
+
+    yield batch;
+
+    currentId = batch[batch.length - 1].id as number;
+  }
+}
+
 export async function* extractCertificateAppDataBatched(
   table: string,
   pkColumn: string,
